a8506371e1743af6f22e6986b30169dedd42870c
vim.cmd <vim.cmd@gmail.com>
2016-06-22 15:05:07 +0500

    added application class

---
+++ src/main/java/blog/Application.java
@@ -0,0 +1,13 @@
+package blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class Application {
+    public static void main(String[] args) throws Exception {
+        ConfigurableApplicationContext ctx = new SpringApplication("/blog/integration.xml").run();
+        System.out.println("Hit Enter to terminate");
+        System.in.read();
+        ctx.close();
+    }
+}
